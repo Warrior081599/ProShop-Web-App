806d2b9fc4151ce812d4c6a5fc469fcb0a940ee4
Azad Raj <arjitchaudhary3@gmail.com>
2024-08-28 17:22:44 +0530

    Added some basic bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to ProShop</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
