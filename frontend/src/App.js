7ccd2208a5ae78253d6c39edaf6e0c5fee6c78c3
Azad Raj <arjitchaudhary3@gmail.com>
2024-09-02 02:16:36 +0530

    commit on 02-09-2024

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome To ProShop</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
